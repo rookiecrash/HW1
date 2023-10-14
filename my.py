32b5cdfd9208e7f186c1708e6a87e794fa7eddad
rookiecrash <1528170838@qq.com>
2023-10-14 17:00:03 +0800

    fistt commit

---
+++ my.py
@@ -0,0 +1,13 @@
+def sum_naturals(n):
+    """return the sum of fist n
+
+        >>> sum_naturals(10)
+        55
+        >>> sum_naturals(100)
+        5050
+    """
+    total, k = 0, 1
+    while k <= n:
+        total, k = total + k, k + 1
+    return total
+
